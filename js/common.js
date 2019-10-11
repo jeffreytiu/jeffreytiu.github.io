297fab99702f490b5fb895c56d02b7a2583dda29
Jeffrey <jeffreytiu629@gmail.com>
2019-10-11 23:15:41 +0800

    index2

--- js/common.js
+++ js/common.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
-      var position = $(this).scrollTop();
-      if(position >= 100) {
-        $('.navbar-expand-md').addClass('custom-navbar');
-      } else {
-        $('.navbar-expand-md').removeClass('custom-navbar');
-      }
-    });
+  $(window).scroll(function(){
+    var position = $(this).scrollTop();
+    if(position >= 100) {
+      $('.navbar-expand-md').addClass('custom-navbar');
+    } else {
+      $('.navbar-expand-md').removeClass('custom-navbar');
+    }
+  });
+  
+  $("img.lazyload").lazyload({
+    effect : "fadeIn",
+    effectTime: 2000
+  });
 });
