98161bc360a02338d5a84b75d9a47e90e9bafa25
Jeffrey <jeffreytiu629@gmail.com>
2019-10-11 20:31:43 +0800

    index

---
+++ js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+      var position = $(this).scrollTop();
+      if(position >= 100) {
+        $('.navbar-expand-md').addClass('custom-navbar');
+      } else {
+        $('.navbar-expand-md').removeClass('custom-navbar');
+      }
+    });
+});
