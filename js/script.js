a2d10b1e9cbec9c1b1d45903c8c05a3a0470cbf4
Jeffrey <jeffreytiu629@gmail.com>
2019-10-23 12:02:32 +0800

    update 20191023

--- js/script.js
+++ js/script.js
@@ -14,6 +14,6 @@ $(document).ready(function(){
 $(function () {
     $(document).scroll(function(){
         var $nav = $("#mainNavBar");
-        $nav.toggleClass("fixed-top scrolled", $(this).scrollTop() > $nav.height());
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
     })
 })
